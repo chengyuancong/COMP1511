d5c133bc4fb60fabc5332d2e5c0e5bcbf758963f
Yuancong <chengyuantsung@gmail.com>
2020-03-15 21:34:20 +1100

    Upload

--- lab08/concat.c
+++ lab08/concat.c
@@ -1,3 +1,4 @@
+//z5285978
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,27 +14,27 @@ int main(int argc, char * argv[]){
     char s2[MAX];
     
     //Assume fgets gets some valid input
-    fgets(s2,MAX,stdin);
+    fgets(s2, MAX, stdin);
     int len = strlen(s2);
     
     //Remove newline if it exists
-    if(len > 0 && s2[len-1] == '\n'){
+    if (len > 0 && s2[len -1 ] == '\n') {
         s2[len-1] = '\0';
     }
         
-    char * s3 = concat(s1,s2);
-    char * s4 = concat(s2,s1);
-    char * s5 = concat("",s2);
-    char * s6 = concat(s2,"");
-    char * s7 = concat(s3,s3);
+    char * s3 = concat(s1, s2);
+    char * s4 = concat(s2, s1);
+    char * s5 = concat("", s2);
+    char * s6 = concat(s2, "");
+    char * s7 = concat(s3, s3);
         
-    printf("s1: %s\n",s1);
-    printf("s2: %s\n",s2);
-    printf("s3: %s\n",s3);
-    printf("s4: %s\n",s4);
-    printf("s5: %s\n",s5);
-    printf("s6: %s\n",s6);
-    printf("s7: %s\n",s7);
+    printf("s1: %s\n", s1);
+    printf("s2: %s\n", s2);
+    printf("s3: %s\n", s3);
+    printf("s4: %s\n", s4);
+    printf("s5: %s\n", s5);
+    printf("s6: %s\n", s6);
+    printf("s7: %s\n", s7);
      
     free(s3);
     free(s4);
